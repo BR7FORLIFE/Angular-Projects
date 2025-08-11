389bab23ea0c29461a77ca747eb0bf69431a9154
br7forlife <diazpachecobrayan@gmail.com>
2025-08-11 17:33:25 -0500

    intent to fix the problem cards

--- memory-match-angular/src/app/table/table.ts
+++ memory-match-angular/src/app/table/table.ts
@@ -1,24 +1,73 @@
 import { Component } from '@angular/core';
 
 interface Slug {
-  id: number
-  imagesrc: string,
-  alt: string
+  id: number;
+  imagesrc: string;
+  alt: string;
 }
 
 const infoSlug: Slug[] = [
-  {id: 1, imagesrc: "https://i.pinimg.com/736x/94/5d/83/945d8320b2991e7345ea0dda5ee1b6bb.jpg", alt: "icono de html"},
-  {id: 2, imagesrc: "https://i.pinimg.com/736x/ac/26/26/ac262626eb1e924c85b8b68fe97c2213.jpg", alt: "icono de css"},
-  {id: 3, imagesrc: "https://i.pinimg.com/736x/0e/4f/dc/0e4fdce8ac22e09688c580e5bc4dcd7d.jpg", alt: "icono de js"},
-  {id: 4, imagesrc: "https://i.pinimg.com/736x/f7/39/92/f739922c635b66d0b4af65443230a1f7.jpg", alt: "imagen de react"},
-  {id: 5, imagesrc: "https://i.pinimg.com/736x/dd/86/e1/dd86e167a140b312fff9f1a86ae2762e.jpg", alt: "icono de python"},
-  {id: 6, imagesrc: "https://i.pinimg.com/736x/6e/46/e7/6e46e7dbe2bb73dacc055e5dbd85c3ad.jpg", alt: "icono de c"},
-  {id: 7, imagesrc: "https://i.pinimg.com/736x/71/8d/ea/718deacc79b8b2e9252a78a08ba0b795.jpg", alt: "icono de java"},
-  {id: 8, imagesrc: "https://i.pinimg.com/736x/62/b8/72/62b8724bc0d855fac10901da7f0e3b57.jpg", alt: "icono de php"},
-  {id: 9, imagesrc: "https://i.pinimg.com/736x/54/a2/43/54a24310f75e8c5b77a88f60776ae4c3.jpg", alt: "icono de ts"},
-  {id: 10, imagesrc:"https://i.pinimg.com/736x/a3/3f/3f/a33f3f15417a2afd5441e9325fa9a546.jpg", alt: "icono de git"},
- ]
-
+  {
+    id: 1,
+    imagesrc:
+      'https://i.pinimg.com/736x/94/5d/83/945d8320b2991e7345ea0dda5ee1b6bb.jpg',
+    alt: 'icono de html',
+  },
+  {
+    id: 2,
+    imagesrc:
+      'https://i.pinimg.com/736x/ac/26/26/ac262626eb1e924c85b8b68fe97c2213.jpg',
+    alt: 'icono de css',
+  },
+  {
+    id: 3,
+    imagesrc:
+      'https://i.pinimg.com/736x/0e/4f/dc/0e4fdce8ac22e09688c580e5bc4dcd7d.jpg',
+    alt: 'icono de js',
+  },
+  {
+    id: 4,
+    imagesrc:
+      'https://i.pinimg.com/736x/f7/39/92/f739922c635b66d0b4af65443230a1f7.jpg',
+    alt: 'imagen de react',
+  },
+  {
+    id: 5,
+    imagesrc:
+      'https://i.pinimg.com/736x/dd/86/e1/dd86e167a140b312fff9f1a86ae2762e.jpg',
+    alt: 'icono de python',
+  },
+  {
+    id: 6,
+    imagesrc:
+      'https://i.pinimg.com/736x/6e/46/e7/6e46e7dbe2bb73dacc055e5dbd85c3ad.jpg',
+    alt: 'icono de c',
+  },
+  {
+    id: 7,
+    imagesrc:
+      'https://i.pinimg.com/736x/71/8d/ea/718deacc79b8b2e9252a78a08ba0b795.jpg',
+    alt: 'icono de java',
+  },
+  {
+    id: 8,
+    imagesrc:
+      'https://i.pinimg.com/736x/62/b8/72/62b8724bc0d855fac10901da7f0e3b57.jpg',
+    alt: 'icono de php',
+  },
+  {
+    id: 9,
+    imagesrc:
+      'https://i.pinimg.com/736x/54/a2/43/54a24310f75e8c5b77a88f60776ae4c3.jpg',
+    alt: 'icono de ts',
+  },
+  {
+    id: 10,
+    imagesrc:
+      'https://i.pinimg.com/736x/a3/3f/3f/a33f3f15417a2afd5441e9325fa9a546.jpg',
+    alt: 'icono de git',
+  },
+];
 
 @Component({
   selector: 'app-table',
@@ -27,4 +76,16 @@ const infoSlug: Slug[] = [
 })
 export class Table {
   protected datalist = infoSlug;
+  protected interrogationImage =
+    'https://cdn-icons-png.flaticon.com/512/5405/5405894.png';
+
+  protected cards: (string | Slug)[] = Array(this.datalist.length).fill(this.interrogationImage)
+
+  revealImage(indexTarget: number){
+    return this.cards[indexTarget] = this.datalist[indexTarget]
+  }
+
+  getImage($index: number): Slug{
+    return this.revealImage($index)
+  }
 }
