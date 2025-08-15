29bdf8ee9aab5eb03e0c567ddd7eaaa24f0052fd
br7forlife <diazpachecobrayan@gmail.com>
2025-08-15 14:01:45 -0500

    started project spotify

---
+++ 03-spotify-clone/src/app/core/Layout/header-layout/header-layout.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-layout',
+  imports: [],
+  templateUrl: './header-layout.html',
+})
+export class HeaderLayout {}
