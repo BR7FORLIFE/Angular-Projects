bab2f99ccffbe91c5f125018530343154dde3a8b
br7forlife <diazpachecobrayan@gmail.com>
2025-08-16 20:40:24 -0500

    create aside layout component for bibliotec of music

---
+++ 03-spotify-clone/src/app/core/Layout/aside-layout/aside-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-layout',
+  imports: [],
+  templateUrl: './aside-layout.html',
+})
+export class AsideLayout {
+
+}
