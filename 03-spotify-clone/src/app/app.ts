f937bfd15cfe9edbfd0723517a871a1f2a95232f
br7forlife <diazpachecobrayan@gmail.com>
2025-08-16 20:43:01 -0500

    modified and delete some archives

--- 03-spotify-clone/src/app/app.ts
+++ 03-spotify-clone/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderLayout } from "./core/Layout/header-layout/header-layout";
+import { AsideLayout } from "./core/Layout/aside-layout/aside-layout";
+import { MainLayout } from "./core/Layout/main-layout/main-layout";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderLayout],
+  imports: [RouterOutlet, HeaderLayout, AsideLayout, MainLayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
