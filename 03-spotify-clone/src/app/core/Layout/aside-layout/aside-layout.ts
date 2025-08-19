5c21c50a3f00bbcdba0462f8a284d68416b7a289
br7forlife <diazpachecobrayan@gmail.com>
2025-08-19 16:25:34 -0500

    some changes

--- 03-spotify-clone/src/app/core/Layout/aside-layout/aside-layout.ts
+++ 03-spotify-clone/src/app/core/Layout/aside-layout/aside-layout.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-aside-layout',
   imports: [],
   templateUrl: './aside-layout.html',
 })
 export class AsideLayout {
+  
+
+ 
 
 }
