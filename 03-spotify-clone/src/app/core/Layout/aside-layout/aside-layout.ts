32f5f565e1d7c33ce5ffa65648ca177038aacde1
br7forlife <diazpachecobrayan@gmail.com>
2025-08-23 20:53:18 -0500

    better logic in aside layout ts

--- 03-spotify-clone/src/app/core/Layout/aside-layout/aside-layout.ts
+++ 03-spotify-clone/src/app/core/Layout/aside-layout/aside-layout.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
-import { Playlistcomponent } from "@app/shared/UI/Buttons/playlistcomponent/playlistcomponent";
+import { Playlistcomponent } from '@app/shared/UI/Buttons/playlistcomponent/playlistcomponent';
+import { listOfMusic } from '@constant/ListOfMusic';
+import { StructurePlaylist } from '@models/playlist.model';
 
 @Component({
   selector: 'app-aside-layout',
   imports: [Playlistcomponent],
   templateUrl: './aside-layout.html',
 })
-export class AsideLayout {}
+export class AsideLayout {
+  public playlist: StructurePlaylist[] = listOfMusic;
+
+  public numberOfPlaylist: number = listOfMusic.length;
+
+  getNumberOfPlaylist(){
+    return Array(this.numberOfPlaylist).fill(0);
+  }
+}
