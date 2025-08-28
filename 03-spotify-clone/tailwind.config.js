924887f41198b34eef321525578137e085f3a0fc
br7forlife <diazpachecobrayan@gmail.com>
2025-08-28 14:52:54 -0500

    configure and add new animations

--- 03-spotify-clone/tailwind.config.js
+++ 03-spotify-clone/tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,ts}', // Tailwind analizará tus archivos Angular
-  ],
+  content: ['./src/**/*.{html,ts}', './src/**/*.html'],
   theme: {
     extend: {
       fontFamily: {
         ibm: ['"IBM Plex Sans"', 'sans-serif'],
       },
+      keyframes: {
+        'fade-in-up': {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.6s ease-out',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
