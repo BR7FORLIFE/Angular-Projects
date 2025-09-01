70e72240d13bdb9dc31d4f5a3b7cb09425a6fcb0
br7forlife <diazpachecobrayan@gmail.com>
2025-09-01 13:12:16 -0500

    refactoricing logic and better practices

--- memory-match-angular/src/app/table/table.ts
+++ memory-match-angular/src/app/table/table.ts
@@ -1,104 +1,55 @@
-import { Component } from '@angular/core';
-
-interface Slug {
-  id: number;
-  imagesrc: string;
-  alt: string;
-}
-
-const infoSlug: Slug[] = [
-  {
-    id: 1,
-    imagesrc:
-      'https://i.pinimg.com/736x/94/5d/83/945d8320b2991e7345ea0dda5ee1b6bb.jpg',
-    alt: 'icono de html',
-  },
-  {
-    id: 2,
-    imagesrc:
-      'https://i.pinimg.com/736x/ac/26/26/ac262626eb1e924c85b8b68fe97c2213.jpg',
-    alt: 'icono de css',
-  },
-  {
-    id: 3,
-    imagesrc:
-      'https://i.pinimg.com/736x/0e/4f/dc/0e4fdce8ac22e09688c580e5bc4dcd7d.jpg',
-    alt: 'icono de js',
-  },
-  {
-    id: 4,
-    imagesrc:
-      'https://i.pinimg.com/736x/f7/39/92/f739922c635b66d0b4af65443230a1f7.jpg',
-    alt: 'imagen de react',
-  },
-  {
-    id: 5,
-    imagesrc:
-      'https://i.pinimg.com/736x/dd/86/e1/dd86e167a140b312fff9f1a86ae2762e.jpg',
-    alt: 'icono de python',
-  },
-  {
-    id: 6,
-    imagesrc:
-      'https://i.pinimg.com/736x/6e/46/e7/6e46e7dbe2bb73dacc055e5dbd85c3ad.jpg',
-    alt: 'icono de c',
-  },
-  {
-    id: 7,
-    imagesrc:
-      'https://i.pinimg.com/736x/71/8d/ea/718deacc79b8b2e9252a78a08ba0b795.jpg',
-    alt: 'icono de java',
-  },
-  {
-    id: 8,
-    imagesrc:
-      'https://i.pinimg.com/736x/62/b8/72/62b8724bc0d855fac10901da7f0e3b57.jpg',
-    alt: 'icono de php',
-  },
-  {
-    id: 9,
-    imagesrc:
-      'https://i.pinimg.com/736x/54/a2/43/54a24310f75e8c5b77a88f60776ae4c3.jpg',
-    alt: 'icono de ts',
-  },
-  {
-    id: 10,
-    imagesrc:
-      'https://i.pinimg.com/736x/a3/3f/3f/a33f3f15417a2afd5441e9325fa9a546.jpg',
-    alt: 'icono de git',
-  },
-];
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { infoSlug, interrogationImageUrl } from '../constants/slags';
+import { Slug } from '../interfaces/Slug';
+import { STATE_CARDS } from '../enums/State';
 
 @Component({
   selector: 'app-table',
   imports: [],
   templateUrl: './table.html',
 })
-export class Table {
-  protected datalist = this.shuffleArray([
-    ...infoSlug, ...infoSlug
-  ]);
-  protected interrogationImage =
-    'https://cdn-icons-png.flaticon.com/512/5405/5405894.png';
+export class Table implements OnInit, OnDestroy {
+  protected cards: Slug[] = [];
+  protected interrogationImage = interrogationImageUrl;
+  protected state: boolean[] = [];
 
-  protected cards: (string | Slug)[] = Array(this.datalist.length).fill(
-    this.interrogationImage
-  );
-
-  revealImage(indexTarget: number) {
-    return (this.cards[indexTarget] = this.datalist[indexTarget]);
+  protected blockTable: boolean = false;
+  ngOnInit(): void {
+    this.cards = [...infoSlug, ...infoSlug];
+    this.state = Array(this.cards.length).fill(false);
   }
 
-  isSlug(card: string | Slug): card is Slug {
-    return (card as Slug).imagesrc !== undefined;
-  }
+  ChangeState(index: number) {
+    if (this.blockTable || this.state[index]) return;
 
-  shuffleArray<T>(array: T[]): T[] {
-    const newArray = [...array];
-    for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    this.state[index] = true;
+
+    const indexToReveal = this.state
+      .map((state, index) => (state ? index : -1))
+      .filter((index) => index !== -1);
+
+    if (indexToReveal.length === 2) {
+      this.blockTable = true;
+      if (this.verifiedPair(indexToReveal)) {
+        this.blockTable = false;
+      } else {
+        setTimeout(() => {
+          this.restartCards(indexToReveal);
+          this.blockTable = false;
+        }, 1000);
+      }
     }
-    return newArray;
   }
+
+  verifiedPair(indexToReveal: number[]): boolean {
+    return this.cards[indexToReveal[0]].id === this.cards[indexToReveal[1]].id;
+  }
+
+  restartCards(indexToReveal: number[]) {
+    for (const idx of indexToReveal) {
+      this.state[idx] = false;
+    }
+  }
+
+  ngOnDestroy(): void {}
 }
