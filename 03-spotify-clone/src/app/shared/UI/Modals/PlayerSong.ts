71432328caa837eec354f9d883326e1a15929732
br7forlife <diazpachecobrayan@gmail.com>
2025-09-04 11:47:57 -0500

    defined logic for modal of the reproductor

--- 03-spotify-clone/src/app/shared/UI/Modals/PlayerSong.ts
+++ 03-spotify-clone/src/app/shared/UI/Modals/PlayerSong.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, OnDestroy } from '@angular/core';
+import { StructurePlaylist } from '@models/playlist.model';
+import { Song } from '@models/song.model';
 
 @Component({
   selector: 'ModalPlayerSong',
   templateUrl: './PlayerSong.html',
 })
-export class PlayerSong {}
+export class PlayerSong implements OnChanges, OnDestroy {
+  @Input() structurePlaylist: StructurePlaylist;
+  @Output() sendBooleanModal = new EventEmitter<boolean>();
+
+  private songsPlaylist: Song[] = [];
+
+  ngOnChanges(): void {
+    if (this.structurePlaylist) {
+      this.songsPlaylist = this.structurePlaylist.songs;
+    }
+  }
+  getAllSongs() {}
+  ngOnDestroy(): void {
+    this.songsPlaylist = [];
+  }
+
+  protected emitBoolean() {
+    this.sendBooleanModal.emit(false);
+  }
+}
