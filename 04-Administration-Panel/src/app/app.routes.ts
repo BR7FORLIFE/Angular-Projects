9cdfb987fb89437aed53e32bc7123400b2b8614e
br7forlife <diazpachecobrayan@gmail.com>
2025-09-21 19:20:59 -0500

    define some rutes

--- 04-Administration-Panel/src/app/app.routes.ts
+++ 04-Administration-Panel/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { App } from './app';
+import { Login } from './features/login/login';
+import { Dashboard } from './features/dashboard/dashboard';
+
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
+];
