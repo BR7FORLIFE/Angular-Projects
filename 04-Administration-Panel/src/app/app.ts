6edf000e5830aeec7d5d448151c85a5b015ea577
br7forlife <diazpachecobrayan@gmail.com>
2025-09-21 19:22:06 -0500

    some configurations and fix

--- 04-Administration-Panel/src/app/app.ts
+++ 04-Administration-Panel/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Login } from './features/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Login]
 })
 export class App {
   protected readonly title = signal('04-Administration-Panel');
