25a937a54e7fb77a8a4687305f4a6475a66bf0fc
br7forlife <diazpachecobrayan@gmail.com>
2025-09-23 14:42:26 -0500

    create component for all dashboard

---
+++ 04-Administration-Panel/src/app/features/menu-dashboard/menu-dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-dashboard',
+  imports: [],
+  templateUrl: './menu-dashboard.html',
+})
+export class MenuDashboard {
+
+}
