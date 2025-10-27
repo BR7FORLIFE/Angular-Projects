0e12066e89309f907c8924c8d562ec1a76c180e7
br7forlife <diazpachecobrayan@gmail.com>
2025-10-27 15:06:06 -0500

    some changes

--- Angular-systemGuard-cookie/src/app/app.routes.ts
+++ Angular-systemGuard-cookie/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/components/login/login';
+import { Dashboard } from './features/auth/components/dashboard/dashboard';
+import { AuthGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [AuthGuard],
+  },
+];
