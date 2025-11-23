f5c61b12ae83a0f84341d254167473c4523003dc
br7forlife <diazpachecobrayan@gmail.com>
2025-11-23 10:35:08 -0500

    create the diferents routes

--- AngularTopics/angularCore/src/app/app.routes.ts
+++ AngularTopics/angularCore/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'signals',
+    loadChildren: () => import('./signals/signal.routes').then((s) => s.SIGNALS_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
