ee0e721a8380113ed6516f8fe8e8ff76ea01e243
br7forlife <diazpachecobrayan@gmail.com>
2025-11-23 10:35:35 -0500

    create the home component

---
+++ AngularTopics/angularCore/src/app/home/components/decoratorsTopic/component-decorator-topic/component-decorator-topic.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-decorator-topic',
+  imports: [],
+  templateUrl: './component-decorator-topic.html',
+})
+export class ComponentDecoratorTopic {
+  
+}
---
+++ AngularTopics/angularCore/src/app/home/components/decoratorsTopic/ng-module-topic/ng-module-topic.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-module-topic',
+  imports: [],
+  templateUrl: './ng-module-topic.html',
+})
+export class NgModuleTopic {
+
+}
