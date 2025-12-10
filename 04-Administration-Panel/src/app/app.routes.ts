3bfab13dff7efd1f69be5c6607acf6d12a91ea6b
br7forlife <diazpachecobrayan@gmail.com>
2025-12-10 14:46:13 -0500

    create and loadchildren of the differents routes

--- 04-Administration-Panel/src/app/app.routes.ts
+++ 04-Administration-Panel/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 
-import { Login } from './features/login/login';
-import { Dashboard } from './features/dashboard/dashboard';
+import Login from '@features/login/login';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.routes').then((route) => route.DASHBOARD_ROUTES),
+  },
   { path: '**', redirectTo: 'login' },
 ];
