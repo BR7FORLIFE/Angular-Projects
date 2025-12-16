88812ea6b2ce34c21bb2d1b3da7525bbdb5e4d95
br7forlife <diazpachecobrayan@gmail.com>
2025-12-16 16:28:39 -0500

    feat: started with the layout

--- 04-Administration-Panel/src/app/features/dashboard/dashboard.component/dashboard.component.ts
+++ 04-Administration-Panel/src/app/features/dashboard/dashboard.component/dashboard.component.ts
@@ -7,14 +7,20 @@ import { FilmService } from '@core/services/film.service';
   selector: 'app-dashboard.component',
   imports: [Statistics],
   template: `
-    <div>
-      <header></header>
-      <div>
-        <aside></aside>
-        <main>
-          <app-statistics [data]="films()"></app-statistics>
-        </main>
-      </div>
+    <div class="w-screen h-screen flex flex-row">
+      <aside class="w-1/5 bg-[#ffffff] flex flex-col justify-between items-center">
+        <div>
+          <img
+            src="https://image.similarpng.com/file/similarpng/original-picture/2021/08/Business-Finance-logo-design-on-transparent-background-PNG.png"
+            alt="business icon"
+          />
+        </div>
+        <div></div>
+        <div></div>
+      </aside>
+      <main class="w-4/5 bg-[#f3f3f3]">
+        <app-statistics [data]="films()"></app-statistics>
+      </main>
     </div>
   `,
 })
