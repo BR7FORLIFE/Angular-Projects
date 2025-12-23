22c08242fe2dac994245c75028241e91999626e1
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-12-22 19:52:08 -0500

    feat: create a constant for the differents images

---
+++ 04-Administration-Panel/src/app/features/menu-dashboard/constants/images.contants.ts
@@ -0,0 +1,37 @@
+type sufixRuteImg = `icons/${string}.svg`;
+
+interface Images {
+  url: sufixRuteImg;
+  alt: string;
+}
+
+export const images: Images[] = [
+  {
+    url: 'icons/decorator.svg',
+    alt: 'menu icon',
+  },
+  {
+    url: 'icons/home.svg',
+    alt: 'home icon',
+  },
+  {
+    url: 'icons/planet.svg',
+    alt: 'planet icon',
+  },
+  {
+    url: 'icons/star.svg',
+    alt: 'star icon',
+  },
+  {
+    url: 'icons/shared.svg',
+    alt: 'shared icon',
+  },
+  {
+    url: 'icons/database.svg',
+    alt: 'database icon',
+  },
+  {
+    url: 'icons/packet.svg',
+    alt: 'packet icon',
+  },
+];
