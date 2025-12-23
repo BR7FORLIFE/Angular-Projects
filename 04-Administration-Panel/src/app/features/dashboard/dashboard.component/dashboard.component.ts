b6c1c2dbe59411dbbc751fd8c97dd5f2650f7149
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-12-22 19:44:17 -0500

    refactor: do some changes

--- 04-Administration-Panel/src/app/features/dashboard/dashboard.component/dashboard.component.ts
+++ 04-Administration-Panel/src/app/features/dashboard/dashboard.component/dashboard.component.ts
@@ -1,26 +1,17 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { Statistics } from '@shared/IU/components/statistics/statistics';
 import { FilmType } from '@core/schemas/film.schema';
 import { FilmService } from '@core/services/film.service';
+import MenuDashboard from '@features/menu-dashboard/menu-dashboard';
+import { Statistics } from '@shared/IU/components/statistics/statistics';
 
 @Component({
   selector: 'app-dashboard.component',
-  imports: [Statistics],
+  imports: [Statistics, MenuDashboard],
   template: `
-    <div class="w-screen h-screen flex flex-row">
-      <aside class="w-1/5 bg-[#ffffff] flex flex-col justify-between items-center">
-        <div>
-          <img
-            src="https://image.similarpng.com/file/similarpng/original-picture/2021/08/Business-Finance-logo-design-on-transparent-background-PNG.png"
-            alt="business icon"
-          />
-        </div>
-        <div></div>
-        <div></div>
-      </aside>
-      <main class="w-4/5 bg-[#f3f3f3]">
-        <app-statistics [data]="films()"></app-statistics>
-      </main>
+    <div class="w-screen h-screen bg-[#949494]">
+      <div class="flex flex-col gap-2 md:flex-row">
+        <app-menu-dashboard></app-menu-dashboard>
+      </div>
     </div>
   `,
 })
@@ -29,6 +20,6 @@ export default class DashboardComponent implements OnInit {
 
   constructor(private film: FilmService) {}
   ngOnInit(): void {
-    this.film.getAllFilms().subscribe((films) => this.films.set(films));
+    //this.film.getAllFilms().subscribe((films) => this.films.set(films));
   }
 }
