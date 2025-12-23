d32e5ddd9a4f5a095cac54a111fe8a3f5ec90274
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-12-22 19:39:10 -0500

    feat: add canActive for protected the route and definied and path default

--- 04-Administration-Panel/src/app/app.routes.ts
+++ 04-Administration-Panel/src/app/app.routes.ts
@@ -2,15 +2,17 @@ import { Routes } from '@angular/router';
 
 import Login from '@features/login/login';
 import Register from '@features/login/register';
+import AuthGuard from '@core/auth/auth.guard';
 
 export const routes: Routes = [
-  //{ path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   {
     path: 'dashboard',
     loadChildren: () =>
       import('./features/dashboard/dashboard.routes').then((route) => route.DASHBOARD_ROUTES),
+    canActivate: [AuthGuard],
   },
   { path: '**', redirectTo: 'login' },
 ];
