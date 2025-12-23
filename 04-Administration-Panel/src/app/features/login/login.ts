8829b38555379f0fe4decdee6146f81a97be9a4b
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-12-22 19:45:25 -0500

    feat: finish the logic register, connect with supabase, and improve styles

--- 04-Administration-Panel/src/app/features/login/login.ts
+++ 04-Administration-Panel/src/app/features/login/login.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { SupabaseClientTs } from '@core/supabase/client/supabase.client.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
+
+interface LoginAuth {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 
 @Component({
   selector: 'login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   template: `
     <section class="min-h-screen flex items-center justify-center bg-gray-100">
       <article class="w-full max-w-md bg-white rounded-2xl shadow-lg px-8 py-10">
@@ -24,10 +38,11 @@ import { Component } from '@angular/core';
         </div>
 
         <!-- Form -->
-        <form class="space-y-4">
+        <form class="space-y-4" [formGroup]="form" (submit)="loginUser()">
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1"> Email </label>
             <input
+              formControlName="email"
               type="email"
               placeholder="you@example.com"
               class="w-full rounded-xl border border-gray-300 px-4 py-2.5 text-sm
@@ -38,12 +53,17 @@ import { Component } from '@angular/core';
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1"> Password </label>
             <input
+              formControlName="password"
               type="password"
               placeholder="••••••••"
               class="w-full rounded-xl border border-gray-300 px-4 py-2.5 text-sm
                  focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-red-300"
             />
           </div>
+
+          @if(MessageHandler().is){
+          <span>{{ MessageHandler().message }}</span>
+          }
 
           <div class="flex justify-between items-center text-sm">
             <label class="flex items-center gap-2 text-gray-600">
@@ -86,10 +106,51 @@ import { Component } from '@angular/core';
         <!-- Footer -->
         <p class="text-center text-sm text-gray-600 mt-6">
           Don’t have an account?
-          <a href="#" class="text-red-500 font-semibold hover:underline"> Sign up </a>
+          <a
+            (click)="goToRegister()"
+            class="text-red-500 font-semibold hover:underline hover:cursor-pointer"
+          >
+            Sign up
+          </a>
         </p>
       </article>
     </section>
   `,
 })
-export default class Login {}
+export default class Login implements OnInit {
+  protected form!: FormGroup<LoginAuth>;
+  protected MessageHandler = signal<{ is: boolean; message?: string }>({ is: false });
+
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private router: Router,
+    private supabase: SupabaseClientTs
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: this.fb.control('', { validators: [Validators.required, Validators.email] }),
+      password: this.fb.control('', { validators: [Validators.required] }),
+    });
+  }
+
+  async loginUser(): Promise<void> {
+    if (!this.form.valid) return;
+
+    const { email, password } = this.form.getRawValue();
+
+    const { data, error } = await this.supabase.client.auth.signInWithPassword({ email, password });
+
+    if (error) {
+      this.MessageHandler.set({ is: true, message: 'Error to login!' });
+      return;
+    }
+    this.MessageHandler.set({ is: true, message: 'Login succesfull!' });
+
+    this.router.navigate(['/dashboard']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+}
