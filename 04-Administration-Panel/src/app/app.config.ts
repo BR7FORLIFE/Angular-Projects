f120a1c6b7e405b9bb3bd7249234dec97478fd9c
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-12-28 14:27:11 -0500

    feat: update HTTP client configuration and clean up global styles

--- 04-Administration-Panel/src/app/app.config.ts
+++ 04-Administration-Panel/src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient , withFetch} from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
@@ -16,6 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideCharts(withDefaultRegisterables()),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
   ],
 };
