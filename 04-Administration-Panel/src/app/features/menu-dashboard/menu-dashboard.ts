9b6a6b9ca493991171d66c879a1268f0c64a4177
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-12-28 19:58:20 -0500

    feat: enhance MenuDashboard and Profile components with improved interactivity and styling

--- 04-Administration-Panel/src/app/features/menu-dashboard/menu-dashboard.ts
+++ 04-Administration-Panel/src/app/features/menu-dashboard/menu-dashboard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { images } from '@features/menu-dashboard/constants/images.contants';
 
 @Component({
@@ -7,15 +7,24 @@ import { images } from '@features/menu-dashboard/constants/images.contants';
   template: `
     <section class="flex h-screen py-2">
       <aside
-        class="fixed inset-y-0 left-0 ml-4 flex h-2/3 w-16 -translate-x-96 flex-col items-center rounded-xl bg-[#a6a6a6] text-black transition-transform md:static md:translate-x-0"
+        class="fixed inset-y-0 left-0 ml-4 flex h-2/3 w-16 flex-col items-center rounded-xl bg-[#a6a6a6] transition-transform duration-300 md:static md:translate-x-0 z-30"
+        [class.-translate-x-96]="!isOpen()"
+        [class.translate-x-0]="isOpen()"
       >
         <article
           class="my-4 flex h-3/4 w-2/3 flex-col items-center justify-center gap-4 rounded-xl  bg-[#242426]"
         >
           @for(image of images; track $index){
           <div>
-            <img [src]="image.url" [alt]="image.alt" class="h-auto w-6 object-contain text-white" />
+            <img
+              (click)="toogle()"
+              [src]="image.url"
+              [alt]="image.alt"
+              class="h-auto w-6 object-contain text-white"
+            />
           </div>
+          } @if (isOpen()) {
+          <div class="fixed inset-0 bg-black/40 z-30 md:hidden" (click)="isOpen.set(false)"></div>
           }
         </article>
       </aside>
@@ -27,4 +36,10 @@ import { images } from '@features/menu-dashboard/constants/images.contants';
 })
 export default class MenuDashboard {
   protected images = images;
+  protected isOpen = signal<boolean>(false);
+
+  toogle() {
+    this.isOpen.update((v) => !v);
+    console.log(this.isOpen())
+  }
 }
--- 04-Administration-Panel/src/app/features/profile/profile.component.ts
+++ 04-Administration-Panel/src/app/features/profile/profile.component.ts
@@ -4,8 +4,8 @@ import { profileOptions } from '@features/profile/constants/profile.constant';
 @Component({
   selector: 'app-profile',
   imports: [],
-  template: ` <section class="ml-4 mt-4 flex h-auto w-full flex-col md:w-72">
-    <article class="mb-4 flex h-14 flex-col">
+  template: ` <section class="mt-4 ml-4 flex h-auto w-full flex-col md:w-72">
+    <article class="flex h-14 flex-col">
       <div class="flex flex-row items-center gap-3">
         <img src="icons/profile.png" alt="profile png" class="h-auto w-8 object-contain" />
 
@@ -16,18 +16,40 @@ import { profileOptions } from '@features/profile/constants/profile.constant';
       </div>
 
       @if (isOpenModal()) {
-      <span>customerpop@gmail.com</span>
+      <span class="ml-11 text-neutral-600">customerpop@gmail.com</span>
       }
     </article>
 
     <article>
       @for (option of profileOptions; track $index) {
-      <section class="mb-4 flex flex-col">
-        <span>{{ option.group }}</span>
+      <section class="flex flex-col">
+        <span class="text-neutral-500">{{ option.group }}</span>
         @for (resource of option.resources; track $index) {
-        <div class="flex flex-row gap-3 rounded-xl p-3">
-          <img [src]="resource.image.url" [alt]="resource.image.alt" />
-          <span>{{ resource.item }}</span>
+        <div
+          class="group relative flex items-center gap-3 p-3 rounded-xl border-2 border-transparent  hover:border-white/60 overflow-hidden"
+        >
+          <div
+            class="absolute inset-0 rounded-xl bg-linear-to-r from-[#aaaaaa] via-[#b0b4b3] to-[#c3ccc9] opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-0"
+          ></div>
+
+          <img class="relative z-10" [src]="resource.image.url" [alt]="resource.image.alt" />
+
+          <span class="relative z-10">
+            {{ resource.item }}
+          </span>
+
+          <span
+            class="
+          relative z-10 ml-auto
+          size-4 rounded-xl
+          bg-white/60 text-xs font-medium
+          inline-flex items-center justify-center
+          opacity-0 translate-x-2
+          group-hover:opacity-100 group-hover:translate-x-0
+          transition-all duration-200"
+          >
+            o
+          </span>
         </div>
         }
       </section>
