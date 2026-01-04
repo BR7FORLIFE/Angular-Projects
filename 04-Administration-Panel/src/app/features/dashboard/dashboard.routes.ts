f866d9f60c25a0a4e8f130ed00b6428c7bbee4c7
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-04 12:10:46 -0500

    feat: add  a children component for inject analytics components

--- 04-Administration-Panel/src/app/features/dashboard/dashboard.routes.ts
+++ 04-Administration-Panel/src/app/features/dashboard/dashboard.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import DashboardComponent from '@features/dashboard/dashboard.component/dashboard.component';
+import Analytics from '@features/analytics/analytics.component';
 
 export const DASHBOARD_ROUTES: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    children: [
+      {
+        path: 'analytics',
+        component: Analytics,
+      },
+    ],
   },
 ];
