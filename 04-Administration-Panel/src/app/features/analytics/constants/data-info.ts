6a4b1566196c69370db935b772ff5d070fb9f015
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-08 20:48:17 -0500

    feat: create the data info chart by use for several components

---
+++ 04-Administration-Panel/src/app/features/analytics/constants/data-info.ts
@@ -0,0 +1,9 @@
+import { AreaParams } from '@features/analytics/types/chart-params';
+
+export const InfoAreaChart: AreaParams[] = [
+  { time: '2025-12-31', value: 10 },
+  { time: '2026-01-01', value: 40 },
+  { time: '2026-01-02', value: 30 },
+  { time: '2026-01-03', value: 80 },
+  { time: '2026-01-04', value: 20 },
+];
