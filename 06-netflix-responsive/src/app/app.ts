6909f6302f4b75fad18e4122f85de093fad89de8
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-15 09:13:15 -0500

    feat: defined the logic for netflix clone

--- 06-netflix-responsive/src/app/app.ts
+++ 06-netflix-responsive/src/app/app.ts
@@ -1,12 +1,37 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { options } from './constants/options';
+import { NgClass } from '@angular/common';
+
+interface MovieInfo {
+  id: number;
+  selector: string;
+  imageUrl: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgClass],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('06-netflix-responsive');
+  protected options = options;
+  protected numberImages = Array.from({ length: 4 });
+
+  protected movieInfo: MovieInfo[] = [
+    {
+      id: 1,
+      selector: 'Reproducir',
+      imageUrl: '/images/play.svg',
+      alt: 'play icon',
+    },
+    {
+      id: 2,
+      selector: 'Más información',
+      imageUrl: '/images/info.svg',
+      alt: 'info icon',
+    },
+  ];
 }
