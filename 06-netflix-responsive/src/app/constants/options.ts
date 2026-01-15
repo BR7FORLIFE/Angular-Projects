b6571faccbb177b00b58758fba3096969958983c
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-15 09:13:49 -0500

    feat: all changes

---
+++ 06-netflix-responsive/src/app/constants/options.ts
@@ -0,0 +1,39 @@
+interface Options {
+  id: number;
+  name: string;
+}
+
+export const options: Options[] = [
+  {
+    id: 1,
+    name: 'Inicio',
+  },
+  {
+    id: 2,
+    name: 'Series',
+  },
+  {
+    id: 3,
+    name: 'Películas',
+  },
+  {
+    id: 4,
+    name: 'Juegos',
+  },
+  {
+    id: 5,
+    name: 'Novedades Populares',
+  },
+  {
+    id: 6,
+    name: 'Mi lista',
+  },
+  {
+    id: 7,
+    name: 'Explora por idiomas',
+  },
+  {
+    id: 8,
+    name: 'Languaje Reactor Catalogue',
+  },
+];
