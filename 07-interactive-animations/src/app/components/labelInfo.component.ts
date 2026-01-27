a3ab4b609ad8ad6e78a3ed62463f23bcab3977db
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-27 16:38:41 -0500

    feat: implement GSAP animations and refactor labelInfo component

--- 07-interactive-animations/src/app/components/labelInfo.component.ts
+++ 07-interactive-animations/src/app/components/labelInfo.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, type ElementRef } from '@angular/core';
 import { NgStyle, NgClass } from '@angular/common';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   imports: [NgStyle, NgClass],
   selector: 'app-label-info',
-  template: ` <section class="w-full flex justify-center items-center" [ngStyle]="{ height }">
+  template: ` <section
+    #scene
+    class="w-full flex justify-center items-center"
+    [ngStyle]="{ height }"
+  >
     <div
+      #label
       class="w-2/3 h-44 flex flex-col items-start justify-center gap-4
                border-orange-400"
       [ngClass]="{
@@ -26,10 +33,41 @@ import { NgStyle, NgClass } from '@angular/common';
     </div>
   </section>`,
 })
-export class InfoLabel {
+export class InfoLabel implements AfterViewInit {
   @Input() title!: string;
   @Input() complement?: string;
   @Input() paragraph!: string;
   @Input() height: string = '72vh';
   @Input() align: 'left' | 'right' = 'left';
+
+  //elementos html
+  @ViewChild('scene', { static: true }) scene!: ElementRef;
+  @ViewChild('label', { static: true }) label!: ElementRef;
+
+  ngAfterViewInit(): void {
+    //animacion del label description
+    gsap.registerPlugin(ScrollTrigger);
+
+    const labelEl = this.label.nativeElement;
+    const sceneEl = this.scene.nativeElement;
+
+    gsap.fromTo(
+      labelEl,
+      {
+        xPercent: this.align === 'right' ? 100 : -100,
+        autoAlpha: 0,
+      },
+      {
+        xPercent: 0,
+        autoAlpha: 1,
+        ease: 'power4.out',
+        scrollTrigger: {
+          trigger: sceneEl,
+          start: 'top 80%',
+          end: 'top 50%',
+          scrub: 1,
+        },
+      },
+    );
+  }
 }
